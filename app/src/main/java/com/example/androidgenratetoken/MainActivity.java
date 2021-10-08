b1da6f4e16e39fa7a1ae007536cf39c190360a20
SahilKhambe <skhambe1999@gmail.com>
2021-10-08 19:00:48 +0530

    first commit

---
+++ app/src/main/java/com/example/androidgenratetoken/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.androidgenratetoken;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.installations.FirebaseInstallations;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.token_notifications);
+
+        FirebaseInstallations.getInstance().getId().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if(task.isSuccessful()){
+                    String token = task.getResult();
+                    textView.setText("Token " + token);
+                }else{
+                    textView.setText(task.getException().getMessage());
+                }
+            }
+        });
+    }
+}
